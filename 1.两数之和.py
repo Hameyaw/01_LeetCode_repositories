b21026336ab9939e2c5039fd37658e6925172025
Hameyaw <1907872480@qq.com>
2026-03-19 14:35:30 +0800

    20260319inital

---
+++ 1.两数之和.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=1 lang=python
+#
+# [1] 两数之和
+#
+# 输入：nums = [2,7,11,15], target = 9
+# 输出：[0,1]
+# 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+        for i, num in enumerate(nums):
+            if target - num in hash_map:
+                return [hash_map[target - num], i]
+            hash_map[num] = i
+        return []
+
+# @lc code=end
+
---
+++ 2.两数相加.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=2 lang=python
+#
+# [2] 两数相加
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# 输入：l1 = [2,4,3], l2 = [5,6,4]
+# 输出：[7,0,8]
+# 解释：342 + 465 = 807.
+# 示例 2：
+
+# 输入：l1 = [0], l2 = [0]
+# 输出：[0]
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy_head = ListNode(0)
+        current = dummy_head
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy_head.next
+    
+
+    
+
+
+
+
+
+        
+
+
+
+        
+# @lc code=end
+
